63645fd063d251bf7dec7cd63a3bac78752777f6
Ad-closeNN <1709301095@qq.com>
2024-10-03 16:29:38 +0800

    Finish

--- WForst-Breeze.py
+++ WForst-Breeze.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 url = "https://profile-counter.glitch.me/WForst_Breeze/count.svg"
-a = 40
-while a > 0:
+a = 0
+while a <= 21100:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
     numbers = ''.join([tspan.get_text() for tspan in soup.find_all('tspan')])
     if response.status_code == 200:
         print(f"[{a}] GET {numbers} OK: "+str(response.status_code))
-        a = a - 1
+        a = a + 1
     else:
         print("Error: "+str(response.status_code))
