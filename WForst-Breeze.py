039bb12e023c8bc703e3d65696376b68b23fab09
Ad-Cat <Ad_closeNN@outlook.com>
2024-10-03 17:50:59 +0800

    Update WForst-Breeze.py

--- WForst-Breeze.py
+++ WForst-Breeze.py
@@ -1,13 +1,15 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 url = "https://profile-counter.glitch.me/WForst_Breeze/count.svg"
 a = 0
-while a <= 20000:
+while a <= 30000:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
     numbers = ''.join([tspan.get_text() for tspan in soup.find_all('tspan')])
     if response.status_code == 200:
         print(f"[{a}] GET {numbers} OK: "+str(response.status_code))
         a = a + 1
+        time.sleep(0.5)
     else:
         print("Error: "+str(response.status_code))
